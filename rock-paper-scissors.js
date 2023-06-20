2fba4ad3c472e955530eedf9e2c5d02e51fade68
MeghanGerbrandt <132425497+MeghanGerbrandt@users.noreply.github.com>
2023-06-19 22:11:51 -0600

    Rock Paper Scissors is done for now. Will go back and add a UI later. Added the the score functionality to the game() function, it now loops to play 5 times and totals up the number of wins, losses, and ties and outputs the winner and scores at the end of the 5 turn round.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -17,26 +17,16 @@ function game()
     loop through playRound() function 5 times in a row
     if player wins, add a point for the player
     if computer wins, add a point for the computer
+    if its a tie, add a point to the tie tally
     after 5 rounds, tally up winner and output as either player or computer
-
-function resetGame() 
-    not specified in assignment, but will add function to reset the game after 5 rounds 
-    and winner chosen, and reset score to 0
-
 */
 
 const computerChoice = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
-let playerWin = 0;
-let computerWin = 0;
-validChoice = 0;
+let tieScore = 0;
+let validChoice = 0;
 
-//const playerSelection = prompt("Choose either Rock, Paper, or Scissors").toLowerCase();
-//const computerSelection = getComputerChoice();
-
-
-//console.log(playRound(playerSelection, computerSelection));
 
 function getComputerChoice() {
     return computerChoice[(Math.floor(Math.random() * computerChoice.length))];
@@ -44,31 +34,48 @@ function getComputerChoice() {
  
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
+        tieScore++;
         return `It's a Tie, you both chose ${playerSelection}!`;
     } else if 
         (playerSelection === "rock" && computerSelection === "scissors" ||
         playerSelection === "paper" && computerSelection === "rock" ||
         playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore++;
             return `You Win, ${playerSelection} beats ${computerSelection}!`;
     } else if 
         (playerSelection === "rock" && computerSelection === "paper" ||
         playerSelection === "paper" && computerSelection ==="scissors" ||
         playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore++;
             return `You Lose, ${computerSelection} beats ${playerSelection}!`;  
     } else if 
         (playerSelection !== "rock" || "paper" || "scissors")  {        
         validChoice--;
-        return "Please select a valid choice.";
+        return "Please select a valid choice.";    
         }  
 }
+
 function game() {
     for(validChoice = 0; validChoice < 5; validChoice++) {
        const playerSelection = prompt("Choose either Rock, Paper, or Scissors").toLowerCase();
        const computerSelection = getComputerChoice();
-       console.log(playRound(playerSelection, computerSelection));
+       console.log(playRound(playerSelection, computerSelection));     
     }
-}
-game();
+    if (playerScore > computerScore) {
+        return "Congrats, you won this round!";
+        }
+       else if (computerScore == computerScore) {
+        return "You tied with the computer on this round!";
+       } 
+       else {
+        return "You Lost. Better luck next time!";
+       }
+}    
+console.log(game());
+console.log("Your Final Score:  \n" + playerScore + " Won \n" + computerScore + " Lost  \n" + tieScore + " Tied");
+
+
+
 
 
 
